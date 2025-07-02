493bbcc686eba9b915894be5d1cea6695b8eb050
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-07-02 13:13:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my_bucket" {
-  name          = "github-action-demo"
+  name          = "github-action-demo-1"
   location      = "US"
   project = "tt-dev-002"
   force_destroy = true
